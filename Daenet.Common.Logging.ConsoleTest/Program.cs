bffbad22cd9508198e66d4a94e87067518e5d469
Holger Vetter <h.vetter@outlook.com>
2017-09-06 14:19:50 +0200

    added console test project

---
+++ Daenet.Common.Logging.ConsoleTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using Daenet.Diagnostics;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+
+namespace Daenet.Common.Logging.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ILoggerFactory loggerFactory = new LoggerFactory();
+            loggerFactory.AddConsole(true);
+
+            LogManager logManager = new LogManager(loggerFactory, "Daenet.Common.Logging.ConsoleTest");
+
+            logManager.TraceError(TracingLevel.Level3, 3, "Hello World-1");
+
+            logManager.AddScope("MachineName", Environment.MachineName); 
+            logManager.TraceError(TracingLevel.Level3, 3, "Hello World-2");
+
+            logManager.AddScope("Property1", "Value1");
+            logManager.TraceError(TracingLevel.Level3, 3, "Hello World-3");
+
+            Console.ReadLine();
+        }
+    }
+}
