21fe71b81fbdd4aab1ae6555754172b825f31c04
Holger Vetter <h.vetter@outlook.com>
2017-09-06 18:36:42 +0200

    added LogManagerOfT for better DI

---
+++ Daenet.Common.Logging/LogManagerOfT.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+
+//TODO: Check Signature
+//TODO: Refactor all
+//TODO: Property for tracing in UTC
+
+namespace Daenet.Common.Logging
+{
+    public class LogManager<T> : LogManager
+    {
+        public LogManager(ILoggerFactory loggerFactory,T type) : base(loggerFactory, type.GetType().FullName)
+        {
+
+        }
+    }
+}
+
