c2b94c74027f2bf6f6ec2521ac3871377ccd9ab2
Holger Vetter <h.vetter@outlook.com>
2017-09-19 11:52:09 +0200

    mark obsolete methods in ILogManager interface, increment version to  0.0.9

--- Daenet.Common.Logging/ILogManager.cs
+++ Daenet.Common.Logging/ILogManager.cs
@@ -6,29 +6,53 @@ namespace Daenet.Common.Logging
     public interface ILogManager
     {
         Dictionary<string, string> CurrentScope { get; }
+
         string SourceName { get; }
 
         void AddAdditionalParams(string paramName, string paramValue);
+
         [Obsolete("Use AddAdditionalParams(string paramName, string paramValue) instead.")]
         void AddScope(string scopeName, string scopeValue);
+
         IDisposable BeginScope<TState>(TState state);
+
         IDisposable BeginScope(string messageFormat, params object[] args);
+
         void ClearScope();
+
         void Close();
+
         void Dispose();
+
         void RemoveAdditionalParams(string paramName);
+
         [Obsolete("Use RemoveAdditionalParams(string paramName) instead.")]
         void RemoveScope(string scopeName);
+
         void TraceCritical(int eventId, Exception err, string msg, params object[] myParams);
+
         void TraceCritical(int eventId, string msg, params object[] myParams);
+
         void TraceDebug(int eventId, string msg, params object[] myParams);
+
+        [Obsolete("Use TraceError(int eventId, Exception err, string msg, params object[] myParams) instead.", false)]
         void TraceError(TracingLevel traceLevel, int eventId, Exception err, string msg, params object[] myParams);
+
+        [Obsolete("Use TraceError(int eventId, string msg, params object[] myParams) instead.", false)]
         void TraceError(TracingLevel traceLevel, int eventId, string msg, params object[] myParams);
+
         void TraceError(int eventId, string msg, params object[] myParams);
+
         void TraceError(int eventId, Exception err, string msg, params object[] myParams);
+
+        [Obsolete("Use TraceMessage(int eventId, string msg, params object[] myParams) instead.", false)]
         void TraceMessage(TracingLevel traceLevel, int eventId, string msg, params object[] myParams);
+
         void TraceMessage(int eventId, string msg, params object[] myParams);
+
+        [Obsolete("Use TraceWarning(int eventId, string msg, params object[] myParams) instead.", false)]
         void TraceWarning(TracingLevel traceLevel, int eventId, string msg, params object[] myParams);
+
         void TraceWarning(int eventId, string msg, params object[] myParams);
     }
 }
